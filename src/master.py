3f6e45f8f6513808c3167b0e64352b050d15a8d9
J. B. <jubi721@gmail.com>
2014-12-10 20:52:18 +0100

    Update master.py
    Programmname berichtigt, Umlaute entfernt

--- src/master.py
+++ src/master.py
@@ -1,8 +1,8 @@
-# ---------------------------------------------------- #
-# Aesol - Programm zum Lösen quadratischer Gleichungen #
-# Erstellt von Julius Bittner    10.12.2014            #
-# Letztes Änderungsdatum         10.12.2014            #
-# -----------------------------------------------------#
+# ----------------------------------------------------- #
+# AesoQ - Programm zum Loesen quadratischer Gleichungen #
+# Erstellt von Julius Bittner    10.12.2014             #
+# Letztes Aenderungsdatum         10.12.2014            #
+# ------------------------------------------------------#
 
 import slave
 import gui
